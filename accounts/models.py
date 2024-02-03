eb254c728186f8459262cc630075bf69a448de08
kakego <funfcia@hotmail.com>
2024-02-03 13:47:11 +0100

    Create superuser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, terms_accepted=False, **extra_fields):
+        if not email:
+            raise ValueError('The email field must be set.')
+        if not terms_accepted:
+            raise ValueError('Accepting the terms is required.')
+        
+        email = self.normalize_email(email)
+        user = self.model(
+                email=email,
+                terms_accepted=terms_accepted,
+                **extra_fields
+            )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('terms_accepted', True)
+        
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    terms_accepted = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+    
+    def has_module_perms(self, app_label):
+        return self.is_superuser
