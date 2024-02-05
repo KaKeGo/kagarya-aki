c867e5dc795b1628c5ab168e259de7e8787f36bc
kakego <funfcia@hotmail.com>
2024-02-05 17:21:19 +0100

    Add first models

--- taskboard/models.py
+++ taskboard/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProjectBoard(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+    
+class Task(models.Model):
+    project_board = models.ForeignKey(ProjectBoard, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+    
+class Subtask(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
