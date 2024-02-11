71c8f23d31aa556622f6cc9e82aa51a577583d3e
kakego <funfcia@hotmail.com>
2024-02-11 12:33:02 +0100

    Add view for csrf token

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
+from django.utils.decorators import method_decorator
+from django.middleware.csrf import get_token
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+
+
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class GetCSRFTokenApiView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def get(self, request, *args, **kwargs):
+        csrf_token = get_token(request)
+
+        if not csrf_token:
+            return Response({'Error': 'CSRFToken has not been retrieved'})
+
+        return Response({'CSRFToken': csrf_token}, status=status.HTTP_200_OK)
