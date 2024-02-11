3d42d7b44e42b75fed26b708e8e98d714a269856
kakego <funfcia@hotmail.com>
2024-02-11 12:41:45 +0100

    add urls for csrftoken

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from .views import *
+from .views import (
+    GetCSRFTokenApiView,
+)
 
 
 
@@ -8,6 +10,6 @@ app_name = 'accounts'
 
 
 urlpatterns = [
-    
+    path('getcsrftoken/', GetCSRFTokenApiView.as_view(), name='getcsrftoken'),
 ]
 
