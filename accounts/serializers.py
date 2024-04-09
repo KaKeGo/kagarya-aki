26a23f4aaa65d1ab36962e0c7c3374769a136822
kakego <funfcia@hotmail.com>
2024-04-09 21:44:49 +0200

    password change serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -65,3 +65,20 @@ class UserLoginSerializer(serializers.Serializer):
         
         data['user'] = user
         return data
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    confirm_new_password = serializers.CharField(required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError('Old password is incorrect')
+        return value
+
+    def validate(self, data):
+        if data['new_password'] != data['confirm_new_password']:
+            raise serializers.ValidationError({'confirm_new_password': 'New password must match'})
+        validate_password_length(data['new_password'])
+        return data
