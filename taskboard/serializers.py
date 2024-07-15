72ce916594efeef29088914dfc79375c2f974d4c
kakego <funfcia@hotmail.com>
2024-07-15 22:54:30 +0200

    Create Task serializer and ProjectBoardDetail. ProjectBoardSerializer description changed to short desc 60 charactesr

--- taskboard/serializers.py
+++ taskboard/serializers.py
@@ -5,18 +5,37 @@ from django.contrib.auth import get_user_model
 from accounts.profile_models import UserProfile
 
 from .models import (
-    ProjectBoard,
+    ProjectBoard, Task,
 )
 
 User = get_user_model()
 
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'name', 'created_at', 'completed_at', 'status', 'priority', 'completed', 'creator', 'slug']
+
 class ProjectBoardSerializer(serializers.ModelSerializer):
     creator = serializers.SerializerMethodField()
+    short_description = serializers.SerializerMethodField()
 
     class Meta:
         model = ProjectBoard
-        fields = ['id', 'name', 'description', 'creator', 'total_tasks', 'completed_tasks', 'slug']
+        fields = ['id', 'name', 'short_description', 'creator', 'total_tasks', 'completed_tasks', 'slug']
 
     def get_creator(self, obj):
         user_profile = UserProfile.objects.get(user=obj.creator)
         return user_profile.username if user_profile.username else obj.creator.email
+    
+    def get_short_description(self, obj):
+        if len(obj.description) > 40:
+            return obj.description[:60] + '...'
+        return obj.description
+
+class ProjectBoardDetailSerializer(serializers.ModelSerializer):
+    task = TaskSerializer(many=True, read_only=True, source='task_set')
+    
+    class Meta:
+        model = ProjectBoard
+        fields = ['id', 'name', 'description', 'task', 'creator', 'total_tasks', 'completed_tasks', 'slug']
